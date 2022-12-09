6314e2a9f73491d17bb67fce8db8f3387d5dfeae
=MindIssawara <=>
2022-12-09 15:10:16 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int x[20],i=0,N=0,sumEven=0,sumOdd=0;
+    do{
+        cout << "Enter an integer: ";
+        cin >> x[i];
+        i++;
+        N++;
+    }while(x[i-1]!=0);
+    i=0;
+    while(i<N){
+        if(x[i]%2==0){
+            sumEven++;
+        } else{
+            sumOdd++;
+        }
+        i++;
+    }
+    cout << "#Even numbers = "<<sumEven-1<<endl;
+    cout << "#Odd numbers = "<<sumOdd;
     return 0;
 }
